316dd27814bffbb81c72d728750e858f1ac21b69
jonasdataloop <jonas.m@dataloop.ai>
2025-12-09 10:43:37 +0200

    Refactor import statements in app.py files to remove relative imports for DOCExtractor and PDFExtractor.

--- apps/pdf_processor/app.py
+++ apps/pdf_processor/app.py
@@ -13,7 +13,7 @@ import nltk
 import transforms
 from utils.extracted_data import ExtractedData
 from utils.config import Config
-from .pdf_extractor import PDFExtractor
+from pdf_extractor import PDFExtractor
 
 logger = logging.getLogger("rag-preprocessor")
 
